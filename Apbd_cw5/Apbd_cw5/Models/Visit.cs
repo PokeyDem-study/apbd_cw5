c1e22cc41b23055dd233bc9ee301caedaeeb2ff1
Aleksandr Maslov <s24439@pjwstk.edu.pl>
2024-04-15 21:24:45 +0200

    Added class Visit and implemented class fields

---
+++ Apbd_cw5/Apbd_cw5/Models/Visit.cs
@@ -0,0 +1,9 @@
+namespace Apbd_cw5.Models;
+
+public class Visit
+{
+    public string Date { get; set; }
+    public Animal Animal { get; set; }
+    public string Description { get; set; }
+    public double Cost { get; set; }
+}
